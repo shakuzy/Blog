8ac01005389c6c7eb3899114122f71ee9e135d74
Shakaaa <mg5839667@gmail.com>
2021-03-22 06:25:05 -0300

    Inicio del proyecto

---
+++ DotNetSeguridad/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace DotNetSeguridad.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ DotNetSeguridad/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNetSeguridad.Startup))]
+namespace DotNetSeguridad
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
